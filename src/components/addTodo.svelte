9d5af3f4b7b8c0e324610644123620d383e23584
Ryan Joseph R. Samonte <samonteryanjoseph@gmail.com>
2021-08-22 15:14:39 +0800

    setup supabase

---
+++ src/components/addTodo.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let todoContent: string;
+</script>
+
+<div class="form mt-5">
+    <input type="text" bind:value={todoContent} placeholder="Add New Task">
+</div>
+
+{todoContent == undefined ? "" : todoContent}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,12 @@
 <script>
     import Navigation from "../components/navigation.svelte";
+    import AddTodo from "../components/addTodo.svelte";
 </script>
 
 <h1 class="page-title">To-Do App</h1>
 
 <Navigation />
+
+<AddTodo />
 
 <slot />
